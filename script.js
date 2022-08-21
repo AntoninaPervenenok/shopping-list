e526cd2438edd50d0b5952de7d24b7181b8ac980
AntaninaPervenenok <110695329+AntaninaPervenenok@users.noreply.github.com>
2022-08-22 01:02:29 +0300

    The first type
    не работает

--- script.js
+++ script.js
@@ -7,3 +7,29 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const sendInput = document.querySelector('#input');
+const itemsList = document.querySelector('#items');
+
+sendInput.addEventListener('keydowm', function(event) {
+    if (event.key == 'Enter') {
+        const messageText = sendInput.value;
+    };
+    const newMessage = document.createElement('div');
+    newMessage.classList.add('message');
+    newMessage.textContent = messageText;
+
+    if (messageText != '') {
+        itemsList.prepend(newMessage);
+    };
+    sendInput.value = '';
+});
+
+const items = document.querySelectorAll('.message');
+
+for (let item of items) {
+    item.addEventListener('click', function() {
+        item.classList.toggle('done');
+    })
+}
+console.log(newMessage);
