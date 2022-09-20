c0daccd78ea236377825b4bd505a55cb1bed82f9
AntaninaPervenenok <110695329+AntaninaPervenenok@users.noreply.github.com>
2022-09-20 17:03:55 +0300

    Работает
    Сделана работа над ошибками

--- script.js
+++ script.js
@@ -11,8 +11,15 @@
 const sendInput = document.querySelector('#input');
 const itemsList = document.querySelector('#items');
 
+sendInput.addEventListener('keydown', function(event) {
     if (event.key == 'Enter') {
         const messageText = sendInput.value;
+        const newMessage = document.createElement('div');
+        newMessage.classList.add('message');
+        newMessage.textContent = messageText;
+        if (messageText != '') {
+            itemsList.prepend(newMessage);
+        };
         sendInput.value = '';
         newMessage.addEventListener('click', function() {
             newMessage.classList.toggle('done');
